2bc903928288ce8da8cf512b06e5bbb89035ef6d
check520 <sxpp316@gmail.com>
2024-03-04 20:13:59 +0800

    [helius] 1 "Due to the limitation of Helius free version, which does not allow calling the API twice simultaneously, the test_enhanced_txn fails. Therefore, a new test_enhanced_history_txn is added."

--- src/lib.rs
+++ src/lib.rs
@@ -158,14 +158,24 @@ mod tests {
     let sigs = ParseTransactionsRequest::from_slice(&sigs);
     let res = client.parse_transaction(&sigs[0]).await?;
     assert!(!res.is_empty());
-    assert!(res[0].timestamp > 0);
+    assert!(res[0].timestamp > 0); 
+    Ok(())
+  }
 
+  #[rstest::rstest]
+  #[tokio::test]
+  #[allow(clippy::unwrap_used)]
+  async fn test_enhanced_history_txn(config: Config) -> color_eyre::Result<()> {
+    if config.client.is_none() {
+      return Ok(());
+    }
+    let client = config.client();  
     let res = client.parsed_transaction_history("M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K").await?;
     assert!(!res.is_empty());
     assert!(res[0].timestamp > 0);
     Ok(())
   }
-
+  
   #[rstest::rstest]
   #[tokio::test]
   async fn webhook(config: Config) -> color_eyre::Result<()> {
